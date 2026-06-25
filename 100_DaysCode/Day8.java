45f886c323a62181993ba064466feb9d75f4a5e3
Sumitmeena4 <sumitmeena9560@gmail.com>
2026-06-25 13:43:59 +0530

    Code Optimization

--- 100_DaysCode/Day8.java
+++ 100_DaysCode/Day8.java
@@ -5,23 +5,32 @@ public class Day8 {
     public static void main(String[] arg){
 
         Strings string = new Strings();
-        String str1 = "Raghu Nandan Shastri";
-       string.countVowel(str1);
+        String str1 = "Raghu Nandan Sharstri";
+       System.out.println(string.highestFrequency(str1));
 
     }
 }
 class Strings{
 
     //Method to remove given character.
-    //Time -- O(n), Space -- O(1).
+    //Time -- O(n^2), Space -- O(n).
     protected String removeChar(String str, char ch){
         
-        String new_str = "";
+        // String new_str = "";
+        // for(int i=0; i<str.length(); i++){
+        //     if(str.charAt(i) != ch)
+        //         new_str += str.charAt(i);
+        // }
+        // String result = str.replace(String.valueOf(ch),""); //T- O(n), S-O(n).
+        // return result;
+
+        //Using StringBuilder
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<str.length(); i++){
             if(str.charAt(i) != ch)
-                new_str += str.charAt(i);
+                sb.append(str.charAt(i));
         }
-        return new_str;
+        return sb.toString();
     }
 
     //Method to count occurrence of a given char in String.
@@ -38,11 +47,21 @@ class Strings{
     //Method to check a String is Anagram.
     public String checkAnagram(String str1, String str2){
   
-        Arrays.sort(str1.toCharArray());
-        Arrays.sort(str2.toCharArray());  
-        if(str1.equals(str2)){
-            return "Anagram";
-        }  
+        // Time -- O(n log n), Space -- O(n).
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        if(str1.length() != str2.length())
+            return "Not Anagram";
+        else{
+            char[] arr1 = str1.toCharArray();
+            char[] arr2 = str2.toCharArray();  
+
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            if(Arrays.equals(arr1, arr2))
+                return "Anagram";
+        }         
         return "Not Anagram";
     }
 
@@ -60,93 +79,132 @@ class Strings{
 
     //Method to check a given char is vowel or consonant.
     public String vowelOrConsonant(char ch){
-
+        
+        // Time -- O(1), Space -- O(1).
+        if(!Character.isLetter(ch))
+            return "Not an alphabet";
         // if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' 
         // || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
 
-        if("aeiousAEIOU".indexOf(ch) != -1)
-            return "Vowel";
+        ch = Character.toLowerCase(ch);
+        String result = ("aeiou".indexOf(ch) != -1) ? "Vowel" : "Consonant";
 
-        return "Consonant";
+        return result;
     }
 
     //Method to find given char is digit or not.
     public String isDigit(char ch){
 
-        if("1234567890".indexOf(ch) != -1)
-            return "Digit";
+        // String result =  (Character.isDigit(ch))? "Digit" : "Non Digit";
 
-        return "Non Digit";
+        String result = (ch>='0' && ch<='9')? "Digit" : "Non Digit";
+
+        return result;
     }
 
     //Method to replace the space with given character.
     public String replaceSpace(String str,char ch){
 
-        String new_str = "";
-        for(int i=0; i<str.length(); i++){
-            if(str.charAt(i) != ' ')
-                new_str += str.charAt(i);
-            else 
-                new_str += ch;
+        // return str.replace(' ',ch);
+        
+        // String new_str = "";
+        // for(int i=0; i<str.length(); i++){
+        //     if(str.charAt(i) != ' ')
+        //         new_str += str.charAt(i);
+        //     else 
+        //         new_str += ch;
+        // }
+        // return new_str;
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : str.toCharArray()){
+            sb.append(c == ' '? ch : c);
         }
-        return new_str;
+        return sb.toString();
     }
 
     public String toLowerCase(String str){
-        return str.toLowerCase();
+
+        StringBuilder sb = new StringBuilder();
+        for(char ch : str.toCharArray()){
+            if(ch>= 'A' && ch<='Z')
+                sb.append((char)(ch+32));
+            else 
+                sb.append(ch);
+        }
+        return sb.toString();
     }
 
     public String convertVowel(String str){
+        // Time -- O(n), Space -- O(n).
 
-        String newString = "";
-        for(int i=0; i<str.length(); i++){
-            if("aeiou".indexOf(str.charAt(i)) != -1){
-                switch(str.charAt(i)){
-                    case 'a' : newString += 'A';
-                        break;
-                    case 'e' : newString += 'E';
-                        break;
-                    case 'i' : newString += 'I';
-                        break;
-                    case 'o' : newString += 'O';
-                        break;
-                    case 'u' : newString += 'U';
-                        break;
-                }
-            }else
-                newString += str.charAt(i);
+        // String newString = "";
+        // for(int i=0; i<str.length(); i++){
+        //     if("aeiou".indexOf(str.charAt(i)) != -1){
+        //         newString += Character.toUpperCase(str.charAt(i));
+        //     }else
+        //         newString += str.charAt(i);
+        // }
+        StringBuilder result = new StringBuilder();
+        for(char c : str.toCharArray()){
+            if("aeious".indexOf(c) != -1)
+                result.append(Character.toUpperCase(c));
+            else
+                result.append(c);
         }
-        return newString;
+        return result.toString();
     }
 
     //Method to delete vowels in a given string.
     public String deleteVowel(String str){
 
-        String newString = "";
-        for(int i=0; i<str.length(); i++){
-            if("aeiouAEIOU".indexOf(str.charAt(i)) == -1){
-                newString += str.charAt(i);
-            }
+        // String newString = "";
+        // for(char ch : str.toCharArray()){
+        //     if("aeiouAEIOU".indexOf(ch) == -1)
+        //         newString += ch;
+        // }
+
+        // We can also use replaceAll("AEIOUaeiou","").
+        StringBuilder sb = new StringBuilder();
+        for(char ch : str.toCharArray()){
+            if("aeiousAEIOU".indexOf(ch) != -1)
+                sb.append(ch);
         }
-        return newString;
+        return sb.toString();
     }
 
     //Count Vowel and Consonant in a string.
-    public void countVowel(String str){
+    public int countVowel(String str){
 
-        int vowel = 0, consonant = 0;
+        int vowel = 0;
         for(int i=0; i<str.length(); i++){
             if("aeiouAEIOU".indexOf(str.charAt(i)) != -1)
                 vowel++;
-
-            else if(str.charAt(i) != ' ') 
-                consonant++;
         }
-        System.out.println("Vowels : " + vowel + " , and Consonants : " + consonant);
+        return vowel;
     }
 
     //Method to find the char who has higest frequency.
     public char highestFrequency(String str){
-        return ' ';
+
+        // Time -- O(n^2), Space -- O(1).
+        int max = 0;
+        char result = str.charAt(0);
+
+        for(int i=0; i<str.length(); i++){
+            int count =1;
+            for(int j=i+1; j<str.length(); j++){
+                if(str.charAt(i) == str.charAt(j))
+                    count ++;
+            }
+
+            if(count>max){
+                max = count;
+                result = str.charAt(i);
+            }
+        }
+
+        return result;
     }
 }
