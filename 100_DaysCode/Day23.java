adc049a2e62e718480209515e2d222afe4a89ffe
Sumitmeena4 <sumitmeena9560@gmail.com>
2026-07-17 17:11:17 +0530

    Commit day 23

---
+++ 100_DaysCode/Day23.java
@@ -0,0 +1,38 @@
+public class Day23 {
+    public static void main(String[] arg){
+        invertedRHPar(7);
+    }
+
+    static void fullParamid(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void invertedRHPar(int n){
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void invertedLHParamid(int n){
+        for(int i=n; i>=1; i--){
+            while(n-i !=0){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
