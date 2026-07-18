8145dd03a5874d2c2bf0c63cf6fe6532194b37e1
Sumitmeena4 <sumitmeena9560@gmail.com>
2026-07-18 12:02:05 +0530

    Optimization

--- 100_DaysCode/Day24.java
+++ 100_DaysCode/Day24.java
@@ -1,9 +1,9 @@
 class Day24{
     public static void main(String[] arg){
-        invertedFullParamid(8);
+        diamondPattern(9);
     }
 
-    static void invertedFullParamid(int n){
+    static void invertedFullPyramid(int n){
         for(int i=n; i>=1; i--){
 
             for(int j=1; j<=n-i; j++){
@@ -15,5 +15,52 @@ class Day24{
             }
             System.out.println();
         }
-    }  
+    }
+    
+    static void rhombusPattern(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=n-1; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void invertedRhombus(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<n; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void diamondPattern(int n){
+        for(int i=1; i<=n; i++){
+            if(i<=n/2+1){
+                for(int j=1; j<=n/2+1-i; j++){
+                    System.out.print(" ");
+                }
+                for(int k=1; k<=i; k++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }else
+            {
+                for(int j=n/2; j>n-i; j--){
+                    System.out.print(" ");
+                }
+                for(int k=1; k<=n-(i-1); k++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
 }
