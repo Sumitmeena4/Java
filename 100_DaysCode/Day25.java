f3d6a5eb587048ade619e85c0520b2713d189124
Sumitmeena4 <sumitmeena9560@gmail.com>
2026-07-21 09:42:30 +0530

    Code optimization

--- 100_DaysCode/Day25.java
+++ 100_DaysCode/Day25.java
@@ -1,5 +1,35 @@
 public class Day25 {
     public static void main(String[] arg){
         
+        Student std = new Student();
+        System.out.println(std.setId(4));
+    }
+}
+
+class Student{
+
+    int id;
+    String name;
+    static String prof;
+
+    public Student()
+    {
+        name = "sumit";
+        prof = "student";
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    public int setId(int id){
+        this.id = id;
+        return id;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getProf(){
+        return prof;
     }
 }
