3b8796bfb3aafec886429f4d2e58f90ad4027d23
Sumitmeena4 <sumitmeena9560@gmail.com>
2026-07-25 20:27:14 +0530

    Day 34 commit

---
+++ 100_DaysCode/Day34.java
@@ -0,0 +1,28 @@
+public class Day34 {
+    public static void main(String[] arg){
+        hourGlassPattern(7);
+    }
+    protected static void hourGlassPattern(int n){
+        for(int i=1; i<=n; i++){
+            if(n/2+1>=i){
+                for(int j=1; j<=i-1; j++){
+                    System.out.print(" ");
+                }
+                for(int k=1; k<=n/2+2-i; k++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            else{
+                for(int j=1; j<=n-i; j++){
+                    System.out.print(" ");
+                }
+                for(int k=n/2+1-){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            
+        }
+    }
+}
