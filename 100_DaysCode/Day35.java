07abca68a56ea08dedcfc25e4702fd328adb4849
Sumitmeena4 <sumitmeena9560@gmail.com>
2026-07-27 22:44:17 +0530

    Day 35 commit

--- 100_DaysCode/Day35.java
+++ 100_DaysCode/Day35.java
@@ -1,7 +1,10 @@
 public class Day35{
     public static void main(String[] arg){
-        int i = 5000;
+        int i = 50000;
         short s = (short) i;
         System.out.println(s + " " + i);
+        long l = 399999999999993l;
+        int j = (int) l;
+        System.out.println(j);
     }
 }
